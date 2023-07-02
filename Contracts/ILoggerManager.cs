88d313b859b066f37837cf7901606e79002a5a79
Kumardotg <kumardotg@gmail.com>
2023-07-02 12:47:34 -0400

    Update ILoggerManager.cs

--- Contracts/ILoggerManager.cs
+++ Contracts/ILoggerManager.cs
@@ -3,6 +3,7 @@
     public interface ILoggerManager
     {
         void LogInfo(string message);
+        
         void LogWarning(string message);
         void LogDebug(string message);
         void LogError(string message);
